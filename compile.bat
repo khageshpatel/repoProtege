d3207aa4bd59ad547093a179f74feec51b95ea6f
Khagesh Patel <kpatel@iitk.ac.in>
2015-06-08 12:11:45 -0400

    intial

---
+++ compile.bat
@@ -0,0 +1,12 @@
+cd owlapi0
+mvn -DskipTests clean install
+cd ..
+cd owalapi1
+mvn -DskipTests clean install
+cd ..
+cd protege-owlapi
+mvn -DskipTests clean install
+cd ..
+cd protege-master
+mvn -DskipTests clean install
+cd ..
