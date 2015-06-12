a8e4a3d8aa8b0fc3760112b00200c508b1f01d5b
Khagesh Patel <kpatel@iitk.ac.in>
2015-06-12 19:16:47 -0400

    compile.bat for windows users

--- compile.bat
+++ compile.bat
@@ -1,12 +1,14 @@
 cd owlapi0
-mvn -DskipTests clean install -Dmaven.javadoc.skip=true
+call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
+cd ..\owlapi1
+call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
+cd ..\protege-owlapi
+call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
+cd ..\protege-master
+call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
 cd ..
-cd owalapi1
-mvn -DskipTests clean install -Dmaven.javadoc.skip=true
-cd ..
-cd protege-owlapi
-mvn -DskipTests clean install -Dmaven.javadoc.skip=true
-cd ..
-cd protege-master
-mvn -DskipTests clean install -Dmaven.javadoc.skip=true
-cd ..
+del protege-master\protege-distribution\target\protege-5.0.0-beta-18-SNAPSHOT-platform-independent\Protege-5.0.0-beta-18-SNAPSHOT\plugins\org.coode.owlviz.jar
+copy owlviz\target\org.coode.owlviz-4.1.6-SNAPSHOT.jar protege-master\protege-distribution\target\protege-5.0.0-beta-18-SNAPSHOT-platform-independent\Protege-5.0.0-beta-18-SNAPSHOT\plugins\org.coode.owlviz-4.1.6-SNAPSHOT.jar
+cd protege-master\protege-distribution\target\protege-5.0.0-beta-18-SNAPSHOT-platform-independent\Protege-5.0.0-beta-18-SNAPSHOT\
+call run.bat
+pause
