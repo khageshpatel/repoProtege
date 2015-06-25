a8c4be1ac53c601544ac8dd142bae69adba1dd69
ishaand <dube.ishaan@gmail.com>
2015-06-25 16:35:23 -0400

    Made compile.bat do the same as compile.sh

--- compile.bat
+++ compile.bat
@@ -6,6 +6,8 @@ cd ..\protege-owlapi
 call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
 cd ..\protege-master
 call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
+cd ..\owlviz
+call mvn -DskipTests clean install -Dmaven.javadoc.skip=true
 cd ..
 del protege-master\protege-distribution\target\protege-5.0.0-beta-18-SNAPSHOT-platform-independent\Protege-5.0.0-beta-18-SNAPSHOT\plugins\org.coode.owlviz.jar
 copy owlviz\target\org.coode.owlviz-4.1.6-SNAPSHOT.jar protege-master\protege-distribution\target\protege-5.0.0-beta-18-SNAPSHOT-platform-independent\Protege-5.0.0-beta-18-SNAPSHOT\plugins\org.coode.owlviz-4.1.6-SNAPSHOT.jar
